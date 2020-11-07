f57b0556c2fd2943d4af69295965d65b224587b7
cody1991 <codytang@tencent.com>
2020-11-07 16:28:49 +0800

    feat: add Class Set methods

--- src/test/set.js
+++ src/test/set.js
@@ -14,3 +14,19 @@ console.log(set.keys);
 console.log(set.values);
 
 console.log(set.size);
+
+set.add(2);
+set.add(3);
+set.add(4);
+const set2 = new CustomSet();
+set2.add(2);
+set2.add(3);
+set2.add(5);
+
+console.log(set.values);
+console.log(set2.values);
+
+console.log(set.union(set2).values);
+console.log(set.intersection(set2).values);
+console.log(set.difference(set2).values);
+console.log(set2.difference(set).values);
