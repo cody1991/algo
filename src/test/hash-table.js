bf5b513dae519918608324e777270d0ff6f9e895
cody1991 <codytang@tencent.com>
2020-11-08 13:19:14 +0800

    feat: add HashTable.splitListMethod

--- src/test/hash-table.js
+++ src/test/hash-table.js
@@ -2,15 +2,23 @@ const HashTable = require('../../lib/HashTable');
 
 const hashTable = new HashTable();
 
-console.log(hashTable.loseHashCode('Jobs'));
-
-hashTable.put('cody', 'codytang');
-hashTable.put('bob', 'fatbob');
-
-console.log(hashTable.items);
-
+hashTable.put('Ana', 'codytang');
+hashTable.put('Donnie', 'fatbob');
 console.log(hashTable.remove('name'));
 
 console.log(hashTable.items);
 
-console.log(hashTable.get('cody'));
+console.log(hashTable.get('Donnie'));
+console.log(hashTable.get('Ana'));
+
+hashTable.remove('Ana');
+
+console.log(hashTable.items);
+console.log(hashTable.get('Ana'));
+console.log(hashTable.get('Donnie'));
+
+hashTable.remove('Donnie');
+
+console.log(hashTable.items);
+console.log(hashTable.get('Ana'));
+console.log(hashTable.get('Donnie'));
