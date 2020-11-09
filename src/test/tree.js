23f42ba03d97993ead006f936351bc374dc28d70
cody1991 <codytang@tencent.com>
2020-11-09 23:57:39 +0800

    feat: modify Tree Class

--- src/test/tree.js
+++ src/test/tree.js
@@ -2,10 +2,14 @@ const Tree = require('../../lib/Tree');
 
 const tree = new Tree();
 
-tree.insert(4);
-tree.insert(1);
-tree.insert(5);
+tree.insert(8);
+tree.insert(2);
+tree.insert(3);
+tree.insert(9);
 
 console.log(tree);
 
 tree.traverse((val) => console.log(val));
+
+console.log(tree.min);
+console.log(tree.max);
